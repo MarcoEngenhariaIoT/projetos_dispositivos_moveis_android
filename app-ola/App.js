26c868f715e931533132e1100996813f0a2aad0e
Marco Aurélio Machado <marcoengenhariaiot@gmail.com>
2025-09-24 11:39:10 -0300

    Aula 4 - aplicativo com contator - código professor

--- app-ola/App.js
+++ app-ola/App.js
@@ -1,11 +1,26 @@
+
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Image, StyleSheet, Text, TextInput, View } from 'react-native';
+import HomeScreen from './HomeScreen';
+import { useState } from 'react';
 
 export default function App() {
+
+  const [novoTexto, setNovoTexto ] = useState('');
+
   return (
     <View style={styles.container}>
-      <Text>Meu primeiro app com React Native!</Text>
-      <StatusBar style="auto" />
+      <Image 
+        source={{uri: 'https://reactnative.dev/img/tiny_logo.png'}}
+        style={{width: 50, height: 50}}
+      />
+      <TextInput
+        placeholder='Digite o texto'
+        onChangeText={novoTexto1 => setNovoTexto(novoTexto1)}
+        value={novoTexto}
+      />
+      <Text>Olá Mundo!</Text>
+      <HomeScreen/>
     </View>
   );
 }
@@ -13,7 +28,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: 'fff',
+    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
